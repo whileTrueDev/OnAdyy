cf98b2f338e61a3e4953d4d48302c9da783070a3
hwasurr <iamsupermazinga@gmail.com>
2020-03-17 10:49:06 +0900

    migration from onadWeb

---
+++ index.ts
@@ -0,0 +1,7 @@
+import TwitchChatCollectorV2 from './src/collector';
+
+
+// const TwitchChatCollector = require('./model/TwitchChatCollector');
+
+const onad = new TwitchChatCollectorV2();
+onad.run();
